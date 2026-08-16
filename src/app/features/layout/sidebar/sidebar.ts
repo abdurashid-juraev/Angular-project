b0de206d901e7c035c2b43cfe3f6e0618d98ea5d
abdurashid-juraev <aj6243434@gmail.com>
2026-08-16 21:44:03 +0500

    fix

---
+++ src/app/features/layout/sidebar/sidebar.ts
@@ -0,0 +1,8 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class SidebarComponent {}
